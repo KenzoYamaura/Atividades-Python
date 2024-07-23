a968021b5e8e078144d8fbbcea1c96a37b80ab3a
KenzoYamaura <167099167+KenzoYamaura@users.noreply.github.com>
2024-07-22 20:57:01 -0400

    exercicio

--- aula/Atividade Orientação/O.Mercado exec2.py
+++ aula/Atividade Orientação/O.Mercado exec2.py
@@ -2,3 +2,38 @@
 que   permita   aos   professores   gerenciar   turmas   e   alunos.   O   sistema   deve   oferecer 
 funcionalidades para cadastrar turmas, adicionar alunos e suas notas, atualizar informações 
 dos alunos, remover alunos e visualizar as informações das turmas e alunos cadastrados.'''
+
+class Escola:
+    def __init__(self, nomeTurma):
+        self.nomeTurma = nomeTurma
+        self.listaDeTurma = []
+    
+    def AdicionarTurma(self, turma):
+        if turma not in self.listaDeTurma:
+            self.listaDeTurma.append(turma)
+        else:
+            print("Aluno Ja existente")
+
+
+class Turma:
+    def __init__(self, nomeAluno):
+        self.nomeAluno = nomeAluno
+        self.alunos = []
+
+    def AdicionarAluno():
+        pass
+
+
+
+
+class Aluno:
+    def __init__(self, nome):
+        self.nome = nome
+        self.notas = []
+    
+    def AddNotas(self, notas):
+        self.notas.append(notas)
+
+
+escola = Escola("Turma A")
+
