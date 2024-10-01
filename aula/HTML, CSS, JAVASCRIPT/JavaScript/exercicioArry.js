7b28a56ca35402ddf3011d9fc790f4648f8d8e61
KenzoYamaura <167099167+KenzoYamaura@users.noreply.github.com>
2024-10-01 19:29:05 -0400

    dsa

--- aula/HTML, CSS, JAVASCRIPT/JavaScript/exercicioArry.js
+++ aula/HTML, CSS, JAVASCRIPT/JavaScript/exercicioArry.js
@@ -3,6 +3,6 @@ var array1 = [1, 2, 3, 4, 5];
 var array2 = [5, 6, 7, 8, 9, 10];
 
 
-var array3 = [new Set([...array1, ...array2])];
+var array3 = [...new Set([...array1, ...array2])];
 
 console.log(array3)
