fc08f050104a30807509897e4e670b648077d18e
KenzoYamaura <167099167+KenzoYamaura@users.noreply.github.com>
2024-10-02 20:29:35 -0400

    asd

--- aula/HTML, CSS, JAVASCRIPT/JavaScript/exercicioDate.js
+++ aula/HTML, CSS, JAVASCRIPT/JavaScript/exercicioDate.js
@@ -1,3 +1,17 @@
 var dataHoje = new Date()
+var dataNascimento = new Date("2000/01/01")
 
-console.log(dataHoje)
+function idade(dataNascimento) {
+    let idade = 0
+    if (dataHoje.getFullYear() >= dataNascimento.getFullYear()) {
+        idade = idade + (dataHoje.getFullYear() - dataNascimento.getFullYear())
+
+    } if (dataHoje.getDate() < dataNascimento.getDate()) {
+        idade -= 1
+
+    } if (dataHoje.getMonth() < dataNascimento.getMonth()) {
+        idade -= 1
+    } return idade
+}
+
+console.log(idade(dataNascimento))
