e8cb00e4f74d9d46b299486cf13bf257bb68fa71
KenzoYamaura <167099167+KenzoYamaura@users.noreply.github.com>
2024-10-07 23:08:08 -0400

    asdasd

--- aula/HTML, CSS, JAVASCRIPT/JavaScript/DOM/exercicio3.js
+++ aula/HTML, CSS, JAVASCRIPT/JavaScript/DOM/exercicio3.js
@@ -1,10 +1,23 @@
 var botao = document.getElementById("Cadastrar")
+var bemVindo = document.getElementById("bemVindo")
+var msg = document.createElement("p")
+
 
 botao.addEventListener("click", function () {
 
     var userName = document.getElementById("user").value
     let userSenha = document.getElementById("password").value
 
-    if(userName == "admin" && userSenha == "admin")
+    msg.innerHTML = ""
 
+    if(userName == "admin" && userSenha == "admin"){
+        msg.innerHTML = "Bem-Vindo ADM";
+        msg.style.color = "green";
+        bemVindo.appendChild(msg);
+    }    
+    else{
+        msg.innerHTML = "Usuário ou Senha Incorreto"
+        msg.style.color = "red";
+        bemVindo.appendChild(msg)
+    }
 })
