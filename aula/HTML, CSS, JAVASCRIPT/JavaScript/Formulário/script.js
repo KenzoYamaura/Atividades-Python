f1405c06adb36eb04be739465fc850b67bc67837
KenzoYamaura <167099167+KenzoYamaura@users.noreply.github.com>
2024-10-16 21:27:14 -0400

    feartured

--- aula/HTML, CSS, JAVASCRIPT/JavaScript/Formulário/script.js
+++ aula/HTML, CSS, JAVASCRIPT/JavaScript/Formulário/script.js
@@ -5,24 +5,74 @@ const email = document.getElementById("email")
 const senha = document.getElementById("senha")
 const senha2 = document.getElementById("senha2")
 
-function showError(input, mensagem){
+function showError(input, mensagem) {
+
     const formControl = input.parentElement
     formControl.className = "form-control error"
     const small = formControl.querySelector("small")
     small.innerHTML = mensagem
+
 }
 
-function checkRequired(listaInput){
+function showSucesso(input, mensagem) {
 
-    listaInput.forEach(function(input) {
-        if(input.value == ""){
+    const formControl = input.parentElement
+    formControl.className = "form-control sucesso"
+
+}
+
+function checkRequired(listaInput) {
+
+    let valido = true
+    listaInput.forEach(function (input) {
+        if (input.value == "") {
             showError(input, "Campo Obrigatório")
+            valido = false
+        } else {
+            showSucesso(input)
         }
     })
+    return valido
 }
 
-form.addEventListener("submit", function(event){
+function checkSize(input, max, min) {
+    let valido = true
+    if (input.value.length < min) {
+        showError(input, `Tente quer no mínimo ${min}`)
+        valido = false
+    } else if (input.value.length > max) {
+        showError(input, `Maior que ${max}`)
+        valido = false
+    }
+    return valido
+}
+
+function checkPassaword(senha, senha2) {
+    if (senha.value !== senha2.value) {
+        showError(senha2, "As senhas não são iguais")
+        return false
+    }
+    return true
+}
+
+form.addEventListener("submit", function (event) {
     event.preventDefault()
 
-    checkRequired([usuario, email, senha, senha2])
+    let isValid = checkRequired([usuario, email, senha, senha2])
+
+    isValid = checkSize(usuario, 15, 3) && isValid
+    isValid = checkSize(senha, 15, 3) && isValid
+
+    isValid = checkPassaword(senha, senha2) && isValid
+
+    if(isValid){
+        const dados = {
+            usuarioNome: usuario.value,
+            usuarioEmail: email.value,
+            usuarioSenha: senha.value
+        } 
+        console.log(dados)           
+
+        window.location.href = "./home.html"
+    }
 })
